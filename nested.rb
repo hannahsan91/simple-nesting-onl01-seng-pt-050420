519d5dfcf41029c1baed00e1bf414f551ea7829a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 01:49:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,11 @@ end
 #    :languages => ["LISP", "C"]
 # }
 
-programmer_hash = {:yukihiro_matsumoto => {known_for: "Ruby", languages: "LISP", "C"}}
 
 	programmer_hash =
  		{
+		programmer_hash = {:yukihiro_matsumoto => {known_for: "Ruby", languages: "LISP", "C"}}
+}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
