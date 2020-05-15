ca5fb96e684f1b75c45be0f33164f78d579bd906
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 01:32:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,6 +70,7 @@ def adding_matz
 				:known_for => "Ruby",
 					:languages => ["LISP", "C"]
 		}
+		programmer_hash = {name: "yukihiro_matsumoto", known_for: "Ruby", languages: "LISP", "C"}
 	programmer_hash =
  		{
         :grace_hopper => {
